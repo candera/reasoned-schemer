32e37d056790dac0bff1e94191e36f32c1738c9d
Craig Andera <candera@wangdera.com>
2011-05-13 17:04:56 -0400

    Added lein-search dev dependency.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject reasoned-schemer "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[org.clojars.technomancy/lein-search "1.0.0-SNAPSHOT"]])
