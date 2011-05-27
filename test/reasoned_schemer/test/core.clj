44b819acbe5054368186431cd24ca6d55ae1e71c
Craig Andera <candera@wangdera.com>
2011-05-27 08:10:04 -0700

    Solved problems 57-61.

--- test/reasoned_schemer/test/core.clj
+++ test/reasoned_schemer/test/core.clj
@@ -79,3 +79,66 @@
   (is (= [:tea :cup]
          (run* [x]
                (teacupo x)))))
+
+(defn set=
+  "Returns true if a and b have the same elements, regardless of order"
+  [a b]
+  (= (set a) (set b)))
+
+(deftest p57
+  (is (set= [[:tea true] [:cup true] [false true]]
+       (run* [r]
+             (exist [x y]
+                     (conde
+                      ((teacupo x) (== true y) s#)
+                      ((== false x) (== true y)))
+                     (== [x y] r))))))
+
+(deftest p58
+  (is (set= [['_.0 '_.1] ['_.0 '_.1]]
+       (run* [r]
+             (exist [x y z]
+                    (conde
+                     ((== y x)
+                      (exist [x] (== z x)))
+                     ((exist [x] (== y x))
+                      (== x z)))
+                    (== [y z] r))))))
+
+(deftest p59
+  (is (set= [[false '_.0] ['_.0 false]]
+       (run* [r]
+            (exist [x y z]
+                   (conde
+                    ((== y x)
+                     (exist [x] (== z x)))
+                    ((exist [x] (== y x))
+                     (== z x)))
+                   (== false x)
+                   (== [y z] r))))))
+
+(deftest p60
+  (is (set= [false]
+       (run* [q]
+             (let [a (== true q)
+                   b (== false q)]
+               b)))))
+
+;; (run* [q]
+;;       (conde
+;;        (u# (== q :a))
+;;        (s# (== q :b))))
+;; => [:b]
+
+(deftest p61
+  (is (set= [false]
+       (run* [q]
+             (let [a (== true q)
+                   b (exist [x]
+                            (== x q)
+                            (== false x))
+                   c (conde
+                      ((== true q))
+                      (s# (== false q)))]
+               b)))))
+
