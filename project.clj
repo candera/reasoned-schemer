f45019e965036443b772d68eb56c1afe726772b3
Craig Andera <candera@wangdera.com>
2011-05-27 08:08:55 -0700

    Try out swank-clojure 1.4.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0-alpha5"]
                  [core.logic "0.6.1-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.3.1"]])
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]])
