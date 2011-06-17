864b76d78f3c7b986cde81d23c7ed251a188f98d
Craig Andera <candera@wangdera.com>
2011-06-17 11:45:50 -0400

    Updating to Clojure 1.3alpha7

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject reasoned-schemer "1.0.0-SNAPSHOT"
   :description "A Clojure implementation of the examples in The Reasoned Schemer using core.logic."
-  :dependencies [[org.clojure/clojure "1.3.0-alpha5"]
+  :dependencies [[org.clojure/clojure "1.3.0-alpha7"]
                  [core.logic "0.6.1-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
   :main reasoned-schemer.main)
