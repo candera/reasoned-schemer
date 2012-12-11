e30e1c0b3f1f32c739a2e38b8c4408bbb23a948b
Frederik De Bleser <frederik@burocrazy.com>
2012-12-11 11:04:04 +0100

    Update to core.logic 0.7.5 and Clojure 1.4.0.
    - Update dependencies in `project.clj`.
    - Update namespace declarations.
    - Replace `exist` with `fresh`.
    - Exclude `is` from core.logic.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject reasoned-schemer "1.0.0-SNAPSHOT"
   :description "A Clojure implementation of the examples in The Reasoned Schemer using core.logic."
-  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
-                 [org.clojure/core.logic "0.6.2-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/core.logic "0.7.5"]]
   :main reasoned-schemer.main)
 
